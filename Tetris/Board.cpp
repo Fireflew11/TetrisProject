4a97fc93ce39947476439125b8c33e37202eece5
YaelYakobovich007 <yaely7888@gmail.com>
2024-01-13 10:03:48 +0200

    clear line functions

--- Tetris/Board.cpp
+++ Tetris/Board.cpp
@@ -78,7 +78,7 @@ bool Board::check_valid_move(const Shape& shape) const
 
 }
 
-void Board:: updateBoard(const Shape& shape)
+void Board:: updateBoard(const Shape& shape)// לבדוק אם ערכי הY ששמתי טובים פה 
 {
 
 	for (int i = 0; i < 4; ++i)
@@ -90,6 +90,52 @@ void Board:: updateBoard(const Shape& shape)
 	}
 }
 
+void Board::clearLine(int index_line)
+{
+//board_game[index_line][i].drawCube(false); // צריכה להביןאיך אני משנה את הציורים של הצורות
+	for (int i = index_line; i > 0; i--)
+	{
+		for (int j = 0; j < gameConfig::GAME_WIDTH; j++)
+		{
+			board_game[i][j].setIsActive(board_game[i - 1][j].getIsActive()); 
+		}
+	}
+	for (int i = 0; i < gameConfig::GAME_WIDTH; i++)
+	{
+		board_game[0][i].setIsActive(false); 
+	}
+}
+
+bool Board::IsLineFull(int index_line)
+{
+	for (int i = 0; i < gameConfig::GAME_WIDTH; i++)
+	{
+		if (board_game[index_line][i].getIsActive() == true)
+			return false; 
+	}
+	return true; 
+}
+
+void  Board::clearFullLines()
+{
+	int numClearedLines = 0;
+	for (int i = 0; i < gameConfig::GAME_HEIGHT; i++)
+	{
+		if (IsLineFull(i))
+		{
+			numClearedLines++; 
+			clearLine(i); 
+		}
+	}
+
+
+}
+/*
+bool Board::isGameOver()
+{
+
+}
+*/
 
 
 
--- Tetris/Board.h
+++ Tetris/Board.h
@@ -4,6 +4,7 @@
 #include"Shape.h" 
 #include "gameConfig.h"
 
+
 class Shape; 
 
 using namespace std;
@@ -19,12 +20,19 @@ public:
 	void display_board(const int startingX);
 	void insert_Shape(const Shape& shape); 
 	Board(int starting_X=0, int starting_Y=0);
+
+
 	Shape* getCurShape() const;
 	void setCurShape(Shape* curShape);
-	const Cube(&getBoardGame() const)[18][12] ;
-	bool check_valid_move(const Shape& shape) const;
+	const Cube(&getBoardGame() const)[18][12];
 
+	bool check_valid_move(const Shape& shape) const;
 	void updateBoard(const Shape& shape);
+
+	void clearLine(int index_line); 
+	bool IsLineFull(int index_line);
+	void clearFullLines();
+	bool isGameOver(); 
 
 
 private: 
